4d284771f309299dbc23a9f0615d6af918b489ad
Loukmane Chaou <72920928+lkmnch@users.noreply.github.com>
2023-11-24 18:48:55 +0100

    updated height  of project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,116 +1,157 @@
 const Projects = () => {
-	const imgUrlSneaktoons = new URL(
-		"../images/sneaktoonsScreenshot.png",
-		import.meta.url
-	).href
-	return (
-		<section id='portfolio' className='bg-orange-100 h-fit'>
-			<div className='container mx-auto p-4 flex flex-col gap-5 '>
-				<div className='flex flex-col gap-5'>
-					<h3 className='text-4xl  font-bold text-lime-950'>Projects</h3>
-					<p className=' text-lime-950 text-lg'>
-						The Projects I build always focus on bringing real value to
-						customers!
-					</p>
-					<ul className='projects-grid text-lime-950 lg:grid lg:grid-cols-8 gap-5'>
-						<li className=' hover:bg-orange-200/30 rounded-xl h-fit w-fit p-5 flex flex-wrap gap-5 col-span-4'>
-							<img
-								src={imgUrlSneaktoons}
-								width={"120"}
-								height={"70"}
-								alt='sneaktoonsScreenshot'
-							/>
-							<div className='projectDetails flex flex-col'>
-								<p className='projectTitle font-bold'>
-									Shop-Applicaton: Sneaktoons
-								</p>
-								<p className='projectDetailsText'>
-									Sneaktoons is a Online-Shop where people can buy custom
-									painted Sneakers.
-								</p>
-								<ul className='projectTechnologies font-light flex flex-wrap gap-1'>
-									<li>
-										<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-											HTML
-										</div>
-									</li>
-									<li>
-										<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-											tailwindcss
-										</div>
-									</li>
-									<li>
-										<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-											TypeScript
-										</div>
-									</li>
-									<li>
-										<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-											Reactjs
-										</div>
-									</li>
-								</ul>
-							</div>
-						</li>
-						<li className='hover:bg-orange-200/30 rounded-xl h-40 p-5 col-span-4'>
-							<p className='projectTitle font-bold'>
-								E-Learning Modul: GuidedOS
-							</p>
-							<ul className='projectTechnologies font-light flex flex-wrap gap-1'>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										HTML
-									</div>
-								</li>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										tailwindcss
-									</div>
-								</li>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										TypeScript
-									</div>
-								</li>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										Reactjs
-									</div>
-								</li>
-							</ul>
-						</li>
-						<li className='hover:bg-orange-200/30 rounded-xl h-40 p-5 col-span-4'>
-							<p className='projectTitle font-bold'>
-								No-Code Tool: Restaurant Menu Creator
-							</p>
-							<ul className='projectTechnologies font-light flex flex-wrap gap-1'>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										HTML
-									</div>
-								</li>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										tailwindcss
-									</div>
-								</li>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										TypeScript
-									</div>
-								</li>
-								<li>
-									<div className='bg-lime-950 rounded-full text-white text-xs flex item-center leading-5 px-3 py-1'>
-										Reactjs
-									</div>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</section>
-	)
+    const imgUrlSneaktoons = new URL(
+        '../images/skeaktoonsScreenshot-16-9.png',
+        import.meta.url
+    ).href
+    const imgUrlGuidedOS = new URL(
+        '../images/GuidedOsScreenshot.png',
+        import.meta.url
+    ).href
+    const imgUrlRestaurantMenu = new URL(
+        '../images/MenuCreatorScreenshot.png',
+        import.meta.url
+    ).href
+    return (
+        <section id="portfolio" className="h-fit bg-orange-100">
+            <div className="container mx-auto flex flex-col gap-5 p-4 ">
+                <div className="flex flex-col gap-5">
+                    <h3 className="text-4xl  font-bold text-lime-950">
+                        Projects
+                    </h3>
+                    <p className=" text-lg text-lime-950">
+                        The Projects I build always focus on bringing real value
+                        to customers!
+                    </p>
+                    <ul className="projects-grid gap-5 text-lime-950 lg:grid lg:grid-cols-8">
+                        <li className=" col-span-4 flex h-fit w-fit flex-wrap gap-5 rounded-xl p-5 hover:bg-orange-200/30 lg:grid lg:grid-cols-5">
+                            <img
+                                src={imgUrlSneaktoons}
+                                width={'120'}
+                                height={'70'}
+                                alt="sneaktoonsScreenshot"
+                                className="col-span-1 rounded-md"
+                            />
+                            <div className="projectDetails flex flex-col gap-5 lg:col-span-4">
+                                <p className="projectTitle font-bold">
+                                    Shop-Applicaton: Sneaktoons
+                                </p>
+                                <p className="projectDetailsText h-40">
+                                    Sneaktoons is a Online-Shop where people can
+                                    buy custom painted Sneakers.
+                                </p>
+                                <ul className="projectTechnologies flex flex-wrap gap-1 font-light">
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            HTML
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            tailwindcss
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            TypeScript
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            Reactjs
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="col-span-4 h-fit w-fit gap-5  rounded-xl p-5  hover:bg-orange-200/30 lg:grid lg:grid-cols-5">
+                            <img
+                                src={imgUrlGuidedOS}
+                                width={'120'}
+                                height={'70'}
+                                alt="sneaktoonsScreenshot"
+                                className="col-span-1 rounded-md"
+                            />
+                            <div className="projectDetails flex flex-col gap-5 lg:col-span-4">
+                                <p className="projectTitle font-bold">
+                                    E-Learning Modul: GuidedOS
+                                </p>
+                                <p className="projectDetailsText h-40">
+                                    GuidedOS is an interactive E-Learning module
+                                    designed to replicate the daily tasks of a
+                                    database developer. The objective is to
+                                    enhance the learning experience by
+                                    simulating real-world scenarios.
+                                </p>
+                                <ul className="projectTechnologies flex flex-wrap gap-1 font-light">
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            HTML
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            tailwindcss
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            TypeScript
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            Reactjs
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className=" col-span-4 flex h-fit w-fit flex-wrap gap-5 rounded-xl p-5 hover:bg-orange-200/30 lg:grid lg:grid-cols-5">
+                            <img
+                                src={imgUrlRestaurantMenu}
+                                width={'120'}
+                                height={'70'}
+                                alt="sneaktoonsScreenshot"
+                                className="col-span-1 rounded-md"
+                            />
+                            <div className="projectDetails flex flex-col gap-5 lg:col-span-4">
+                                <p className="projectTitle font-bold">
+                                    No-Code Tool: Restaurant Menu Creator
+                                </p>
+                                <p className="projectDetailsText h-40">
+                                    This is an application for Restaurant-Owners
+                                    to create their own menues through a drag
+                                    and drop application.
+                                </p>
+                                <ul className="projectTechnologies flex flex-wrap gap-1 font-light">
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            HTML
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            tailwindcss
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            TypeScript
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="item-center flex rounded-full bg-lime-950 px-3 py-1 text-xs leading-5 text-white">
+                                            Reactjs
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Projects
