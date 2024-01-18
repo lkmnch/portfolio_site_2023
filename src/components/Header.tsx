3ace90c1012438cdc8627bb6b5d0eb552c234f9d
Loukmane Chaou <72920928+lkmnch@users.noreply.github.com>
2024-01-18 20:49:39 +0100

    Added Construction Banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@ import Nav from './Nav'
 const Header = () => {
     return (
         <header className="sticky top-0 z-10 bg-orange-100 ">
+            <div className="bg-red-700 text-center text-white">
+                This Page is currently Under Construction
+            </div>
             <div className="container mx-auto flex items-center justify-between  px-4 py-8">
                 <h1 className="text-xl font-bold text-lime-950 ">
                     <a href="#">Loukmane Chaou</a>
